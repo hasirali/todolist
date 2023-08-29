4a051227ec468b84219f6ae4ae7734cdcead5a83
hasirali <hasira804@gmail.com>
2023-08-29 19:34:37 +0530

    initalize

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,17 @@ import React from 'react';
 const App = () => {
   return (
     <>
-    
+      <div className="app-container">
+        <div className="card main-card">
+          <div className="card-header">
+            <h1 className="card-title">TodoList</h1>
+          </div>
+          <div className="input-container">
+            <input type="text" className="input-field" placeholder="Enter your note" onChange={inputEvent} value={inputList} />
+            <button className="add-button" onClick={addItems} >+</button>
+          </div>
+        </div>
+      </div>
     </>
   )
 };
