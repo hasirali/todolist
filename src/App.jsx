4348dae1aeda063772194b514d9fdcc793438c31
hasirali <hasira804@gmail.com>
2023-08-30 18:53:05 +0530

    project completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,65 @@
 import React, { useState } from 'react';
 import List from './List';
-const App = () => {
 
+const App = () => {
+  // State to manage the input field value and the list of items
   const [inputList, setInputList] = useState("");
   const [items, setItems] = useState([]);
-  // intiialize with an empty array as we get input it get store in the array
 
+  // Input change handler to update inputList state
   const inputEvent = (event) => {
     setInputList(event.target.value);
   };
+
+  // Function to delete an item by its index
   const deleteItems = (id) => {
     setItems((oldItems) => {
       return oldItems.filter((arrEle, index) => {
-        return index !== id;
-      })
-    })
-  }
+        return index !== id; // Filter out the item with the specified index
+      });
+    });
+  };
 
+  // Function to add a new item to the list
   const addItems = () => {
     setItems((oldItems) => {
-      return [...oldItems, inputList]
-    })
-    setInputList("");
-  }
+      return [...oldItems, inputList]; // Add the new inputList value to the list
+    });
+    setInputList(""); // Clear the input field
+  };
+
   return (
     <>
       <div className="app-container">
         <div className="card main-card">
-          {/* <div> */}
           <h1 className="card-title">TodoList</h1>
-          {/* </div> */}
           <div className="input-container">
-            <input type="text"
+            <input
+              type="text"
               className="input-field"
               placeholder="Enter your note"
               onChange={inputEvent}
               value={inputList}
             />
-            <button className="add-button" onClick={addItems} > + </button>
+            <button className="add-button" onClick={addItems}>+</button>
           </div>
           <ul className="todo-list">
             {items.map((itemval, index) => {
-              return <List text={itemval}
-                key={index}
-                id={index}
-                onSelect={deleteItems}
-              />
+              // Render each list item using the List component
+              return (
+                <List
+                  text={itemval}
+                  key={index}
+                  id={index}
+                  onSelect={deleteItems} // Pass the deleteItems function as a prop
+                />
+              );
             })}
           </ul>
         </div>
       </div>
     </>
-  )
+  );
 };
+
 export default App;
--- src/List.jsx
+++ src/List.jsx
@@ -1,17 +1,16 @@
 import React from 'react';
 
 const List = (props) => {
-    
-    return (
-        <>
-        <div className="cross-button" onClick={()=>{
-            props.onSelect(props.id);
-        }}>
-
+  return (
+    <>
+      <div className="cross-button" onClick={() => {
+          // When the cross button is clicked, call the onSelect function with the item's index
+          props.onSelect(props.id);
+      }}>
         <li>{props.text}</li>
-        </div>
-        </>
-
-    )
+      </div>
+    </>
+  );
 };
+
 export default List;
